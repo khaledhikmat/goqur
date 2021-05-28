9db23b3390994ccdb1a455a374c823981d498d52
Khaled Hikmat <khaled.hikmat@gmail.com>
2021-05-28 16:29:15 -0500

    Added Uploads folder

--- prepare-ayahs-4-upload.ps1
+++ prepare-ayahs-4-upload.ps1
@@ -1,5 +1,5 @@
 $srcfile = "./quran-ayahs.csv"
-$destfile = "./quran-ayahs-2-upload.csv"
+$destfile = "././uploads/quran-ayahs-2-upload.csv"
 # Force double-quotation in CSV file 
 Import-CSV $srcfile | Export-CSV $destfile
 # Remove the first line of the file introduced by the above step :-) 
