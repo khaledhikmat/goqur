0db3f3589cb5d2bd230d6eca645b881ff4382229
Khaled Hikmat <khaled.hikmat@gmail.com>
2021-05-28 16:40:49 -0500

    Added temp file

--- prepare-ayahs-4-upload.ps1
+++ prepare-ayahs-4-upload.ps1
@@ -1,6 +1,6 @@
 $srcfile = "./quran-ayahs.csv"
-$destfile = "././uploads/quran-ayahs-2-upload.csv"
+$destfile = "./uploads/quran-ayahs-2-upload.csv"
 # Force double-quotation in CSV file 
 Import-CSV $srcfile | Export-CSV $destfile
 # Remove the first line of the file introduced by the above step :-) 
-(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfilen
+(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfile
