cb27960c0cf7957cbd222f2c4368d233dd9ecfa1
Khaled Hikmat <khaled.hikmat@gmail.com>
2021-05-28 16:31:28 -0500

    Removed temp file

--- prepare-ayahs-4-upload.ps1
+++ prepare-ayahs-4-upload.ps1
@@ -3,4 +3,4 @@ $destfile = "././uploads/quran-ayahs-2-upload.csv"
 # Force double-quotation in CSV file 
 Import-CSV $srcfile | Export-CSV $destfile
 # Remove the first line of the file introduced by the above step :-) 
-(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfile
+(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfilen
