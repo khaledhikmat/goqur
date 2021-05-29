6dc822a575ba3d5a74d1eb7e0196f3036a880f36
Khaled Hikmat <khaled.hikmat@gmail.com>
2021-05-29 17:31:52 -0500

    Fixed Powershell script not to remove the first line

--- prepare-ayahs-4-upload.ps1
+++ prepare-ayahs-4-upload.ps1
@@ -3,4 +3,5 @@ $destfile = "./uploads/quran-ayahs-2-upload.csv"
 # Force double-quotation in CSV file 
 Import-CSV $srcfile | Export-CSV $destfile
 # Remove the first line of the file introduced by the above step :-) 
-(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfile
+# WARNING: Powershell on the server (where Github action runs) behaves differently....it seems like it does not add a new line!!! 
+#(Get-Content $destfile | Select-Object -Skip 1) | Set-Content $destfile
